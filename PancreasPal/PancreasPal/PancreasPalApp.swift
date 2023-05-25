d6e4d6bfcd42df879bed0d19d16809f70b5b1e78
James Greiner <jamesdgreiner@icloud.com>
2023-05-24 18:34:23 -0700

    Initial commit with project scaffolding

---
+++ PancreasPal/PancreasPal/PancreasPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PancreasPalApp.swift
+//  PancreasPal
+//
+//  Created by James Greiner on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PancreasPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
